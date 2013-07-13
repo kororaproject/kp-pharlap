e8cfcd1ffd90565a6c770cc2f75bc77148867605
firnsy <firnsy@securixlive.com>
2013-07-13 21:47:34 +1000

    updated: make yumcache an iterable hash.

--- upstream/KororaDrivers/YumCache.py
+++ upstream/KororaDrivers/YumCache.py
@@ -1,8 +1,12 @@
+import fnmatch
 import json
 import yum
 
 class YumCache(object):
   def __init__(self, yb=yum.YumBase()):
+    if not isinstance(yb, yum.YumBase):
+      raise Exception('Expected YumBase object.')
+
     self._yb = yb
 
     # we're a cache after all
@@ -22,13 +26,24 @@ class YumCache(object):
 
       self._c[p.name].installed = p
 
-    json_data = open('/tmp/modaliases.json')
+    maps = ['/usr/share/korora-drivers-common/yum-modalias.map',
+            '/tmp/modaliases.json']
 
-    _data = json.load(json_data)
+    _map_data = None
+    for m in maps:
+      try:
+        raw_data = open(m)
+        _map_data = json.load(raw_data)
+        break
+      except Exception:
+        pass
 
-    for p,v in _data.iteritems():
-      if p in self._c:
-        self._c[p].record_set('modaliases',  v['modaliases'])
+    if _map_data is not None:
+      for p,v in _map_data.iteritems():
+        if p in self._c:
+          self._c[p].record_set('modaliases',  v['modaliases'])
+    else:
+      print "No modalias maps available."
 
   def total_candidates(self):
     return len(self._candidates)
@@ -51,6 +66,45 @@ class YumCache(object):
 
     return self._c[name].installed is not None
 
+  def search_installed(self, name):
+    found = []
+
+    for p in self._installed:
+      if fnmatch.fnmatch(p.name, name):
+        found.append( self._c[p.name] )
+
+    return found
+
+  def __len__(self):
+    return len(self._c)
+
+  def __contains__(self, key):
+    return key in self._c
+
+  def __getitem__(self, key):
+    if not key in self._c:
+      raise KeyError('Package %s not found in cache.' % key)
+
+    return self._c[key]
+
+  def __iter__(self):
+    return iter(self._c)
+
+  def items():
+    return self._c.items()
+
+  def itervalues(self):
+    return iter(self._c)
+
+  def keys(self):
+    return self._c.keys()
+
+  def values(self):
+    return self._c.values()
+
+  def itervalues(self):
+    return self._c.itervalues()
+
 
 class YumCachePackage(object):
   def __init__(self, name='', candidate=None, installed=None):
@@ -62,6 +116,18 @@ class YumCachePackage(object):
   @property
   def name(self):
     return self._name
+
+  @property
+  def shortname(self):
+    return self._name
+
+  @property
+  def pkname(self):
+    repo = 'installed'
+    if self._installed is None:
+      repo = self.candidate.repoid
+
+    return '%s;%s-%s;%s;%s' % (self._name, self.candidate.version, self.candidate.release, self.candidate.arch, repo)
 
   @name.setter
   def name(self, name):
@@ -91,4 +157,7 @@ class YumCachePackage(object):
 
   def record_set(self, name, value):
     self._records[name] = value
+
+  def is_installed(self):
+    return self._installed is not None
 
